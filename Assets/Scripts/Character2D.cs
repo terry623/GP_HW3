0343e405fa48c8d412a3efcda40cc22a40f1ce16
terry623 <terry92516@gmail.com>
2018-11-29 10:18:41 +0800

    finish character basic move

---
+++ Assets/Scripts/Character2D.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2D : MonoBehaviour
+{
+    private float maxSpeed = 4.0f;
+    private float jumpForce = 200.0f;
+    private bool airControl = true;
+    bool facingRight;
+
+    public LayerMask groundLayer;
+    Transform groundCheck;
+    float groundRadius;
+    bool onGround;
+
+    Animator anim;
+
+    void Awake()
+    {
+        groundCheck = transform.Find("GroundCheck");
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        facingRight = true;
+        groundRadius = 0.1f;
+        onGround = false;
+    }
+
+    void FixedUpdate()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+
+        // change the character animation by onGround state
+        // anim.SetBool("onGround", onGround);
+    }
+
+    public void Move(float movingSpeed, bool jump)
+    {
+        if (onGround || airControl)
+        {
+            // change the character animation by moving speed
+            // anim.SetFloat("Speed", Mathf.Abs(movingSpeed));
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movingSpeed * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if (movingSpeed > 0 && !facingRight || movingSpeed < 0 && facingRight) Flip();
+        }
+
+        if (onGround && jump)
+        {
+            // anim.SetBool("onGround", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpForce));
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 characterScale = transform.localScale;
+        characterScale.x *= -1;
+        transform.localScale = characterScale;
+    }
+}
---
+++ Assets/Scripts/Character2DControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2DControl : MonoBehaviour
+{
+    private Character2D character;
+    private bool jump;
+    private float movingSpeed;
+
+    void Start()
+    {
+        character = GetComponent<Character2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) jump = true;
+    }
+
+    void FixedUpdate()
+    {
+        movingSpeed = Input.GetAxis("Horizontal");
+        character.Move(movingSpeed, jump);
+        jump = false;
+    }
+}
