be999f0d688db472cf6b22bf1fc5d126acebc408
terry623 <terry92516@gmail.com>
2018-11-29 22:38:02 +0800

    connect each scene

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
     public GameObject horse;
+    private int limitNum = 5;
+    int count = 0;
 
     void Start()
     {
-        InvokeRepeating("HorseGo", 0.5f, 1.0f);
+        InvokeRepeating("HorseGo", 0.1f, 1.0f);
     }
 
     void Update()
@@ -18,6 +21,19 @@ public class GameControl : MonoBehaviour
 
     void HorseGo()
     {
-        Instantiate(horse, new Vector3(2.0f, 0, 0), Quaternion.Euler(Vector3.zero));
+        count++;
+        if (count > limitNum)
+        {
+            Invoke("Fade", 2.0f);
+        }
+        else
+        {
+            Instantiate(horse, new Vector3(2.0f, 0, 0), Quaternion.Euler(Vector3.zero));
+        }
+    }
+
+    void Fade()
+    {
+        Initiate.Fade("Stage2", Color.black, 1.0f);
     }
 }
