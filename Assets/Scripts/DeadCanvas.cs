d03d659563b78d297481d9facf4578655be38c46
terry623 <terry92516@gmail.com>
2018-11-29 19:17:22 +0800

    add dead & press any key to restart

---
+++ Assets/Scripts/DeadCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadCanvas : MonoBehaviour
+{
+    private Canvas dead;
+
+    void Start()
+    {
+        dead = GetComponent<Canvas>();
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            dead.enabled = false;
+            Time.timeScale = 1;
+        }
+    }
+}
